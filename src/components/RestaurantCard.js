8e289b63a1882e2798d73817514fda0e506f747f
abinash <154217568+abinash-shrestha@users.noreply.github.com.>
2024-12-29 14:08:58 +0545

    updated css

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -6,11 +6,16 @@ const RestaurantCard = (props) => {
   const { resData } = props;
   const { cloudinaryImageId, name, cuisines, avgRating } = resData?.info;
   const { loggedInUser } = useContext(UserContext);
+
   return (
-    <div className="res-card m-4 p-2 w-[230px] bg-gray-300 rounded-lg">
-      <img className="res-logo rounded-md " src={CDN_URL + cloudinaryImageId} />
+    <div className="res-card m-4 p-2 w-[230px] h-[350px] bg-gray-300 rounded-lg flex flex-col justify-between">
+      <img
+        className="res-logo rounded-md object-cover w-full h-[150px]"
+        src={CDN_URL + cloudinaryImageId}
+        alt={name}
+      />
       <h3 className="font-bold py-1">{name}</h3>
-      <h3>{cuisines.join(', ')}</h3>
+      <h3 className="text-sm text-gray-600">{cuisines.join(', ')}</h3>
       <h3>{avgRating} stars</h3>
       <h3 className="font-bold">User: {loggedInUser}</h3>
     </div>
@@ -20,14 +25,14 @@ const RestaurantCard = (props) => {
 export const withOpenLabel = (RestaurantCard) => {
   return (props) => {
     return (
-      <div>
+      <div className="relative">
         <label className="absolute bg-black text-white p-2 rounded-lg">
           Open
         </label>
-
         <RestaurantCard {...props} />
       </div>
     );
   };
 };
+
 export default RestaurantCard;
